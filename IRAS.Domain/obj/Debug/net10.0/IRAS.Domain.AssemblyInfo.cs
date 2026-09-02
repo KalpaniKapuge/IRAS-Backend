2e3f60eaa2e9057aec9698b7d1625fdd0ab8c56b
Kalpani Kapuge <kalpanikapuge1020@gmail.com>
2026-09-03 02:47:56 +0530

    fix(skill-plans): only allow removing draft evidence✏️

--- IRAS.Domain/obj/Debug/net10.0/IRAS.Domain.AssemblyInfo.cs
+++ IRAS.Domain/obj/Debug/net10.0/IRAS.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IRAS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8162225af77d291607078ddd8772b394c13322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab09b4f0ec9fe649b16df74ce8320a7dbd6b7ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("IRAS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IRAS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
